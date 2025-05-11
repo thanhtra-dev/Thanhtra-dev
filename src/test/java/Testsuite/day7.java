c09c6ca5b8bad9a7e62bbf40406d5985e8f49966
Thanhtra <thanhtraneu17@gmail.com>
2025-05-11 19:19:21 +0700

    Push code lên

--- src/test/java/Testsuite/day7.java
+++ src/test/java/Testsuite/day7.java
@@ -3,8 +3,14 @@ package Testsuite;
 import org.testng.annotations.Test;
 
 public class day7 {
+    @Test
     public void nhapMang(){
 
     }
+
     @Test
+    public void tinhtongmang(){
+
+    }
+
 }
--- src/test/java/Testsuite/day8.java
+++ src/test/java/Testsuite/day8.java
@@ -1,4 +1,48 @@
 package Testsuite;
+import org.jspecify.annotations.Nullable;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+import automation.common.commonbase;
 
-public class day8 {
+public class day8 extends commonbase {
+    String URL = "https://alada.vn/tai-khoan/dang-nhap.html";
+    @Test
+    public void locateElementById()
+    {
+        driver = initChromeBrowser(URL);
+        WebElement txtUsername = driver.findElement(By.id("txtLoginUsername"));
+        System.out.println("Element username: " + txtUsername.getText());
+    }
+    @Test
+    public void locateElementByName()
+    {
+        driver = initChromeBrowser(URL);
+        WebElement txtPass = driver.findElement(By.name("txtLoginPassword"));
+        System.out.println("Element pass: " + txtPass.getText());
+    }
+    @Test
+    public void locateElementByLinkText()
+    {
+        driver = initChromeBrowser(URL);
+        WebElement linkForgotPass = driver.findElement(By.linkText("Quên mật khẩu?"));
+        System.out.println("Element link forgot pass: " + linkForgotPass.getText());
+
+        WebElement linkNoAccount = driver.findElement(By.partialLinkText("Chưa có tài khoản"));
+        System.out.println("Element link no account: " + linkNoAccount.getText());
+    }
+    @Test
+    public void locateElementByClasName_TagName()
+    {
+        driver = initChromeBrowser(URL);
+        WebElement btHopTacGD = driver.findElement(By.className("btn-hoptac"));
+        System.out.println("Element bt hop Tac GD: " + btHopTacGD.getText());
+
+        WebElement btDangNhap = driver.findElement(By.className("btn-submit"));
+        System.out.println("Element bt DangNhap: " + btDangNhap.getText());
+    }
+
 }
+
