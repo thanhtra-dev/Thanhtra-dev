a0881144a9e18e7d41110094c728c093981a5cd4
Thanhtra <thanhtraneu17@gmail.com>
2025-05-11 19:13:51 +0700

    push code

--- src/test/java/Testsuite/btvn2_day8.java
+++ src/test/java/Testsuite/btvn2_day8.java
@@ -1,4 +1,74 @@
 package Testsuite;
 
-public class btvn2_day8 {
+import automation.common.commonbase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class btvn2_day8 extends commonbase {
+    String URL = "https://automationfc.github.io/basic-form/index.html";
+    @Test
+    public void NameById()
+    {
+        driver = initChromeBrowser(URL);
+        WebElement txtName = driver.findElement(By.id("name"));
+        System.out.println("Element Name");
+    }
+
+    @Test
+    public void NameByname()
+    {
+        driver = initChromeBrowser(URL);
+        WebElement txtName = driver.findElement(By.name("name"));
+        System.out.println("Element Name");
+    }
+
+    @Test
+    public void AddressById()
+    {
+        driver = initChromeBrowser(URL);
+        WebElement txtAddress = driver.findElement(By.id("address"));
+        System.out.println("Element Address");
+    }
+
+    @Test
+    public void AddressByname()
+    {
+        driver = initChromeBrowser(URL);
+        WebElement txtAddress = driver.findElement(By.name("address"));
+        System.out.println("Element Address");
+    }
+
+    @Test
+    public void EmailById()
+    {
+        driver = initChromeBrowser(URL);
+        WebElement txtEmail = driver.findElement(By.id("email"));
+        System.out.println("Element Email");
+    }
+
+    @Test
+    public void EmailByname()
+    {
+        driver = initChromeBrowser(URL);
+        WebElement txtEmail = driver.findElement(By.name("email"));
+        System.out.println("Element Email");
+    }
+
+    @Test
+    public void PasswordById()
+    {
+        driver = initChromeBrowser(URL);
+        WebElement txtPassword = driver.findElement(By.id("password"));
+        System.out.println("Element Password");
+    }
+
+    @Test
+    public void PasswordByname()
+    {
+        driver = initChromeBrowser(URL);
+        WebElement txtPassword = driver.findElement(By.name("password"));
+        System.out.println("Element Password");
+    }
+
 }
--- src/test/java/Testsuite/btvn_day8.java
+++ src/test/java/Testsuite/btvn_day8.java
@@ -1,4 +1,66 @@
 package Testsuite;
 
-public class btvn_day8 {
+import automation.common.commonbase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+public class btvn_day8 extends commonbase {
+    String URL = "https://selectorshub.com/xpath-practice-page/";
+
+    @Test
+    public void UserEmailByXpath() {
+        driver = initChromeBrowser(URL);
+        WebElement txtUserEmail = driver.findElement(By.xpath("//label[text()='User Email']"));
+        System.out.println("Element User Email");
+    }
+
+    @Test
+    public void PasswordByXpath() {
+        driver = initChromeBrowser(URL);
+        WebElement txtPassword = driver.findElement(By.xpath("//label[text()='Password']"));
+        System.out.println("Element Password");
+    }
+
+    @Test
+    public void Company() {
+        driver = initChromeBrowser(URL);
+        WebElement txtCompany = driver.findElement(By.xpath("//label[text()='Company']"));
+        System.out.println("Element Company");
+    }
+
+    @Test
+    public void MobileNumber() {
+        driver = initChromeBrowser(URL);
+        WebElement txtMobileNumber = driver.findElement(By.xpath("//label[text()='Mobile Number']"));
+        System.out.println("Element Mobile Number");
+    }
+
+    @Test
+    public void UserEmailByCssSelector() {
+        driver = initChromeBrowser(URL);
+        WebElement txtUserEmail = driver.findElement(By.cssSelector("label[for='User Email']"));
+        System.out.println("Element User Email");
+    }
+
+    @Test
+    public void PasswordByCssSelector() {
+        driver = initChromeBrowser(URL);
+        WebElement txtPassword = driver.findElement(By.cssSelector("label[for='Password']"));
+        System.out.println("Element Password");
+    }
+
+    @Test
+    public void CompanyByCssSelector() {
+        driver = initChromeBrowser(URL);
+        WebElement txtCompany = driver.findElement(By.cssSelector("label[for='Company']"));
+        System.out.println("Element Company");
+    }
+
+    @Test
+    public void MobileNumberByCssSelector() {
+        driver = initChromeBrowser(URL);
+        WebElement MobileNumber = driver.findElement(By.cssSelector("label[for='Mobile Number']"));
+        System.out.println("Element Mobile Number");
+    }
 }
